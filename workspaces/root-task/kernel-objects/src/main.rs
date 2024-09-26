2f4e2a2364c23ece16cf77bfd8c906a0744a4a7d
Nick Spinale <nick@nickspinale.com>
2024-09-26 01:18:49 -0700

    3.A

--- workspaces/root-task/kernel-objects/src/main.rs
+++ workspaces/root-task/kernel-objects/src/main.rs
@@ -11,6 +11,16 @@ use sel4_root_task::root_task;
 
 #[root_task]
 fn main(bootinfo: &sel4::BootInfoPtr) -> ! {
+    sel4::debug_println!("untyped:");
+    for ut_desc in bootinfo.untyped_list() {
+        sel4::debug_println!(
+            "    paddr: {:#016x?}, size bits: {:02?}, is device: {:?}",
+            ut_desc.paddr(),
+            ut_desc.size_bits(),
+            ut_desc.is_device()
+        );
+    }
+
     sel4::debug_println!("TEST_PASS");
 
     sel4::init_thread::suspend_self()
