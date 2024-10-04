2c196ac43a8aeeecd76338ecd0800a7600aec278
Nick Spinale <nick@nickspinale.com>
2024-10-04 13:26:45 -0700

    5.C

--- workspaces/root-task/serial-device/src/main.rs
+++ workspaces/root-task/serial-device/src/main.rs
@@ -40,6 +40,14 @@ fn main(bootinfo: &sel4::BootInfoPtr) -> ! {
 
     let device_ut_cap = bootinfo.untyped().index(device_ut_ix).cap();
 
+    trim_untyped(
+        device_ut_cap,
+        device_ut_desc.paddr(),
+        SERIAL_DEVICE_MMIO_PADDR,
+        empty_slots.next().unwrap(),
+        empty_slots.next().unwrap(),
+    );
+
     sel4::debug_println!("TEST_PASS");
 
     sel4::init_thread::suspend_self()
@@ -55,4 +63,35 @@ fn find_largest_kernel_untyped(bootinfo: &sel4::BootInfo) -> sel4::cap::Untyped 
         .unwrap();
 
     bootinfo.untyped().index(ut_ix).cap()
+}
+
+fn trim_untyped(
+    ut: sel4::cap::Untyped,
+    ut_paddr: usize,
+    target_paddr: usize,
+    free_slot_a: sel4::init_thread::Slot,
+    free_slot_b: sel4::init_thread::Slot,
+) {
+    let rel_a = sel4::init_thread::slot::CNODE
+        .cap()
+        .absolute_cptr(free_slot_a.cptr());
+    let rel_b = sel4::init_thread::slot::CNODE
+        .cap()
+        .absolute_cptr(free_slot_b.cptr());
+    let mut cur_paddr = ut_paddr;
+    while cur_paddr != target_paddr {
+        let size_bits = (target_paddr - cur_paddr).ilog2().try_into().unwrap();
+        ut.untyped_retype(
+            &sel4::ObjectBlueprint::Untyped { size_bits },
+            &sel4::init_thread::slot::CNODE
+                .cap()
+                .absolute_cptr_for_self(),
+            free_slot_b.index(),
+            1,
+        )
+        .unwrap();
+        rel_a.delete().unwrap();
+        rel_a.move_(&rel_b).unwrap();
+        cur_paddr += 1 << size_bits;
+    }
 }
