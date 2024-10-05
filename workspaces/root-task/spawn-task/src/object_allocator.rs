47ec7155afd537f150eabd00bd0337d6ed730d65
Nick Spinale <nick@nickspinale.com>
2024-10-05 01:38:57 -0700

    7.A

--- workspaces/root-task/spawn-task/src/object_allocator.rs
+++ workspaces/root-task/spawn-task/src/object_allocator.rs
@@ -6,14 +6,16 @@
 
 use core::ops::Range;
 
-pub(crate) struct ObjectAllocator {
+pub(crate) struct ObjectAllocator<'a> {
+    bootinfo: &'a sel4::BootInfo,
     empty_slots: Range<usize>,
     ut: sel4::cap::Untyped,
 }
 
-impl ObjectAllocator {
-    pub(crate) fn new(bootinfo: &sel4::BootInfo) -> Self {
+impl<'a> ObjectAllocator<'a> {
+    pub(crate) fn new(bootinfo: &'a sel4::BootInfo) -> Self {
         Self {
+            bootinfo,
             empty_slots: bootinfo.empty().range(),
             ut: find_largest_kernel_untyped(bootinfo),
         }
@@ -50,6 +52,47 @@ impl ObjectAllocator {
     ) -> sel4::Cap<T> {
         self.allocate(T::object_blueprint(size_bits)).cast()
     }
+
+    pub(crate) fn recklessly_allocate_at(
+        &mut self,
+        blueprint: sel4::ObjectBlueprint,
+        paddr: usize,
+    ) -> sel4::cap::Unspecified {
+        let (ut_ix, ut_desc) = self
+            .bootinfo
+            .untyped_list()
+            .iter()
+            .enumerate()
+            .find(|(_i, desc)| {
+                (desc.paddr()..(desc.paddr() + (1 << desc.size_bits()))).contains(&paddr)
+            })
+            .unwrap();
+
+        let ut_cap = self.bootinfo.untyped().index(ut_ix).cap();
+
+        trim_untyped(
+            ut_cap,
+            ut_desc.paddr(),
+            paddr,
+            self.allocate_slot(),
+            self.allocate_slot(),
+        );
+
+        let obj_slot = self.allocate_slot();
+
+        ut_cap
+            .untyped_retype(
+                &blueprint,
+                &sel4::init_thread::slot::CNODE
+                    .cap()
+                    .absolute_cptr_for_self(),
+                obj_slot.index(),
+                1,
+            )
+            .unwrap();
+
+        obj_slot.cap()
+    }
 }
 
 fn find_largest_kernel_untyped(bootinfo: &sel4::BootInfo) -> sel4::cap::Untyped {
@@ -62,4 +105,35 @@ fn find_largest_kernel_untyped(bootinfo: &sel4::BootInfo) -> sel4::cap::Untyped 
         .unwrap();
 
     bootinfo.untyped().index(ut_ix).cap()
+}
+
+fn trim_untyped(
+    ut: sel4::cap::Untyped,
+    ut_paddr: usize,
+    target_paddr: usize,
+    free_slot_a: sel4::init_thread::Slot,
+    free_slot_b: sel4::init_thread::Slot,
+) {
+    let rel_a = sel4::init_thread::slot::CNODE
+        .cap()
+        .absolute_cptr(free_slot_a.cptr());
+    let rel_b = sel4::init_thread::slot::CNODE
+        .cap()
+        .absolute_cptr(free_slot_b.cptr());
+    let mut cur_paddr = ut_paddr;
+    while cur_paddr != target_paddr {
+        let size_bits = (target_paddr - cur_paddr).ilog2().try_into().unwrap();
+        ut.untyped_retype(
+            &sel4::ObjectBlueprint::Untyped { size_bits },
+            &sel4::init_thread::slot::CNODE
+                .cap()
+                .absolute_cptr_for_self(),
+            free_slot_b.index(),
+            1,
+        )
+        .unwrap();
+        rel_a.delete().unwrap();
+        rel_a.move_(&rel_b).unwrap();
+        cur_paddr += 1 << size_bits;
+    }
 }
